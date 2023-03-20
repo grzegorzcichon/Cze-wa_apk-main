a8d3b141dc72076554e83e5debf15fc58bd30273
grzegorzcichon <grzegorz.cichon@outlook.com>
2023-03-20 22:08:19 +0100

    Add  BLoC to C6H6

--- lib/views/aircondition/main/bloc/air_condition_city_main_page_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'air_condition_city_main_page_bloc.dart';
-
-@immutable
-abstract class AirConditionCityMainPageState {}
-
-class AirConditionCityMainPageInitial extends AirConditionCityMainPageState {}
